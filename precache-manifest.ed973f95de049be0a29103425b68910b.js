691b058369c0384b9cdd8330cbe1edd83c03cc43
Anthony Olores <anthony@ABL.local>
2019-07-31 14:30:50 +1200

    Updates

---
+++ precache-manifest.ed973f95de049be0a29103425b68910b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62831775ae740b39fdce8ab8a6abe943",
+    "url": "/currency-rates/index.html"
+  },
+  {
+    "revision": "1fac6eea7147a7daa642",
+    "url": "/currency-rates/static/css/main.0917ae80.chunk.css"
+  },
+  {
+    "revision": "ab557f8c773275088f43",
+    "url": "/currency-rates/static/js/2.ab98bf66.chunk.js"
+  },
+  {
+    "revision": "1fac6eea7147a7daa642",
+    "url": "/currency-rates/static/js/main.5c46ac60.chunk.js"
+  },
+  {
+    "revision": "368986ff4bc0ea3accd5",
+    "url": "/currency-rates/static/js/runtime~main.db12023e.js"
+  }
+]);
